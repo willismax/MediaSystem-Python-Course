6e5fcfd00ba5052e3f1805169fc269811dcc2c85
willismax <willismax.com@gmail.com>
2022-12-25 00:35:49 +0800

    Update config.py

--- line-bot-fly-flask/config.py
+++ line-bot-fly-flask/config.py
@@ -1,1 +1,10 @@
- 
+CHANNEL_ACCESS_TOKEN = ""
+CHANNEL_SECRET = ""
+LINE_USER_ID = ''
+OPENAI_API_KEY = ''
+
+HACKMD_USER_NAME = ''
+IMGUR_CLIENT_ID = ''
+HACKMD_API_TOKEN = ''
+TODO_NOTE_ID = '' #  paste your fixed TODO note id
+TEMP_NOTE_ID = '' # paste your fixed TEMP note id
