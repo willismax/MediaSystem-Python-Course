17416e4a606e852e2a58e0c8f1b4c09d3d8d5720
willismax <willismax.com@gmail.com>
2022-12-27 15:03:05 +0800

    Add openai answer to hackmd

--- line-bot-fly-flask/config.py
+++ line-bot-fly-flask/config.py
@@ -1,11 +1,11 @@
-
 CHANNEL_ACCESS_TOKEN = ""
 CHANNEL_SECRET = ""
 LINE_USER_ID = ''
+
 OPENAI_API_KEY = ''
+IMGUR_CLIENT_ID = ''
 
 HACKMD_USER_NAME = ''
-IMGUR_CLIENT_ID = ''
 HACKMD_API_TOKEN = ''
 TODO_NOTE_ID = '' #  paste your fixed TODO note id
 TEMP_NOTE_ID = '' # paste your fixed TEMP note id
