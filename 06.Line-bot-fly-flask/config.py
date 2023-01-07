182670d5b88759915b5c0c2f01606549c4e7cc13
willismax <willismax.com@gmail.com>
2023-01-07 10:07:33 +0800

    Fix to put config sample

---
+++ 06.Line-bot-fly-flask/config.py
@@ -0,0 +1,10 @@
+CHANNEL_ACCESS_TOKEN = ""
+CHANNEL_SECRET = ""
+LINE_USER_ID = ''
+OPENAI_API_KEY = ''
+IMGUR_CLIENT_ID = ''
+HACKMD_USER_NAME = ''
+HACKMD_API_TOKEN = ''
+TODO_NOTE_ID = '' #  paste your fixed TODO note id
+TEMP_NOTE_ID = '' # paste your fixed TEMP note id
+AI_NOTE_ID = '' # paste your fixed OpenAI note id
