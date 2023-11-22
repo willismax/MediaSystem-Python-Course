c5fcb3d097e7de6fba4742cc64474a85c28ae5b2
willismax <willismax.com@gmail.com>
2023-11-22 22:33:57 +0800

    Update Selenium

--- 04.Selenium/yahoo_selenium.py
+++ 04.Selenium/yahoo_selenium.py
@@ -1,23 +1,24 @@
 #@title Yahoo搜尋 "霍華德"
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument('--headless') # 無頭模式
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+wd = webdriver.Chrome(options=chrome_options)
 url='https://tw.yahoo.com/'
 wd.get(url)
 
 # 操作網頁元素
 element = wd.find_element(By.ID, 'header-search-input')
-key_word = '霍華德'
+key_word = 'OpenAI'
 element.send_keys(key_word)
 wd.find_element(By.ID, 'header-desktop-search-button').click()
 
