68d56d4f7ff4974ca293e5b0dc37d6cfb01a1c88
willismax <willismax.com@gmail.com>
2023-12-30 13:07:02 +0800

    Fix Pytest Demo

--- 07.Pytest-DEMO/test_mogodb_crud/fixtures.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from mongomock.mongo_client import MongoClient
-
-
-@pytest.fixture(name="conn")
-def mongo_client_fixture() -> MongoClient:
-    with MongoClient() as conn:
-        yield conn
